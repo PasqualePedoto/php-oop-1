8f763fc900a61100a9fb06f9ab06f2e3b371a9c3
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-09 14:21:24 +0200

    Complete

--- index.php
+++ index.php
@@ -12,7 +12,47 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
 
 class Movie
 {
+    // Properties
+    public $id;
+    public $title;
+    public $genre;
+    public $release;
+    public $language;
+    public $plot;
+
+    // Costruttore
+
+    public function __construct($id,$title,$genre,$release,$language,$plot)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->release = $release;
+        $this->language = $language;
+        $this->plot = $plot;
+    }
+
+    // Methods
+
+    public function getFullMovie()
+    {
+        $movie = "Movie #$this->id : <br />title: $this->title
+        <br />genre: $this->genre<br />release: $this->release
+        <br />language: $this->language<br />plot: $this->plot.";
+        return $movie;
+    }
 
 }
+
+$movie1 = new Movie(1,"Il signore degli anelli","fantasy","12-09-2020","italian","Parla di anelli di cipolla");
+$movie2 = new Movie(1,"Il signore degli agnelli","romantico","27-05-2014","english","Parla di agnelli da allevamento");
+
+$full_movie_1 = $movie1->getFullMovie();
+$full_movie_2 = $movie2->getFullMovie();
+
+echo $full_movie_1;
+echo "<br />";
+echo "<br />";
+echo $full_movie_2;
 
 ?>
